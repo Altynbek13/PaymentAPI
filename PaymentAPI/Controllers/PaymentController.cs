b17b76fde2b1c6a7a3bdfd1e4411663bcd29ce00
Altynbek <aton.zhumabergen@mail.ru>
2024-12-06 18:28:36 +0500

    controller realisation

---
+++ PaymentAPI/Controllers/PaymentController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentAPI.Dtos;
+using PaymentAPI.Interfaces;
+
+namespace PaymentAPI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IPaymentService _paymentService;
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+        [HttpGet("payment")]
+        public async Task<IActionResult> GetAll()
+        {
+            var payment= await _paymentService.GetAll();
+            return Ok(payment);
+        }        
+        [HttpGet("payment/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var payment = await _paymentService.GetById(id);
+            if (payment ==null )
+            {
+                return NotFound();
+            }
+            return Ok(payment);
+        }
+        [HttpPost("payment")]
+        public async Task<IActionResult> Create([FromBody]PaymentDetailsDto payment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var createId = await _paymentService.Create(payment);
+            return CreatedAtAction(nameof(GetById),new {id= createId},payment);
+        }
+        [HttpPut("payment/{id}")]
+        public async Task<IActionResult> Update([FromBody]PaymentDetailsDto payment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await _paymentService.Update(payment);
+             return NoContent();
+        }
+        [HttpDelete("payment")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _paymentService.Delete(id);
+            return NoContent();
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
